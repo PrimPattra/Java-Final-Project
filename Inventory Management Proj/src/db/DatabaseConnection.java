ba233cc5413a4b1245705b5e343bde511ae307d0
PrimPattra <pimpattra.leh@gmail.com>
2025-04-14 21:38:07 +0700

    Final1

--- Inventory Management Proj/src/db/DatabaseConnection.java
+++ Inventory Management Proj/src/db/DatabaseConnection.java
@@ -4,12 +4,17 @@ import java.sql.*;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/inventory_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "FinalProj108";
     private static Connection connection = null;
 
     private DatabaseConnection() {}
 
     public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
         if (connection == null || connection.isClosed()) {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
         }
